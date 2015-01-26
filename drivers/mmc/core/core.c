fc64c69fe2de16c05c286d80561fc57329d41fd6
Scorpio92 <scorpio92@mail.ru>
2015-01-26 13:45:23 +1000

    fix /drivers/mmc/core/core.c

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -84,13 +84,8 @@ MODULE_PARM_DESC(
  * Internal function. Schedule delayed work in the MMC work queue.
  */
 /*we want mmc_schedule_delayed_work to run in source code of msm_sdcc*/
-#ifdef CONFIG_HUAWEI_KERNEL
-int mmc_schedule_delayed_work(struct delayed_work *work,
-				     unsigned long delay)
-#else
 static int mmc_schedule_delayed_work(struct delayed_work *work,
 				     unsigned long delay)
-#endif
 {
 	return queue_delayed_work(workqueue, work, delay);
 }
